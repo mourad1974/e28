5c5019b8bc73b5efdc745bedea68bc5a9a9c17f1
mourad <mourad_saleh@harvard.edu>
2019-10-26 11:56:57 -0400

    deploying p2 to proDigital Ocean Server

---
+++ P2/src/components/MyGuess.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Your Guess number {{rank}} is : {{range}}</p>
+  </div>
+</template>
+
+
+<script>
+module.exports = {
+  data: function() {
+    return {};
+  },
+
+  props: ["range", "rank"],
+  template: "#my-guess"
+};
+</script>
+
+
+<style>
+</style>
