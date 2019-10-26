6d68ebc0e130af7229d8073a5d8363df6267b2d3
mourad <mourad_saleh@harvard.edu>
2019-10-26 15:33:09 -0400

    deploy to github

---
+++ P2/src/components/NewOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Your try #{{rank}} is : {{range}}</p>
+  </div>
+</template>
+
+
+
+<script>
+module.exports = {
+  data: function() {
+    return {};
+  },
+
+  props: ["range", "rank"],
+  template: "#new-one"
+};
+</script>
