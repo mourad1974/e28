fbb1b803fd063f3ecd62b2d428c5d5ee67ce872c
mourad <mourad_saleh@harvard.edu>
2019-11-08 23:42:50 -0500

    ZipFood added

---
+++ ZipFood/src/components/ShowProduct.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="product">
+    <div class="product-name">{{product.name}}</div>
+    <img
+      class="product-thumb"
+      :alt="' Product image of ' + product.name"
+      :src="require('../assets/images/products/'+ product.id + '.jpg')"
+    />
+    <p class="product-description">{{product.description}}</p>
+    <div class="product-price">${{product.price}}</div>
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "ShowProduct",
+  props: ["product"]
+};
+</script>
+
+
+
+<style scoped>
+</style>
