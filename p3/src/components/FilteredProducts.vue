b373bc0a842e3c3fbef8ebcf55bf67d49bd853f6
mourad <mourad_saleh@harvard.edu>
2019-11-16 19:30:12 -0500

    p3 added

---
+++ p3/src/components/FilteredProducts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Filtered Recipes</h2>
+    <div id='filtered'>
+      <router-link to='{name: "product", params: { "id" :product.id}}'>
+        <ul class='cleanList'>
+          <li v-for='product in FilteredRecipes' :key='product.id'>
+            <p>{{ product.name }}</p>
+            <br />
+            <img :src='require("./../assets/images/products/" + product.id + ".jpg")' />
+          </li>
+        </ul>
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+import { products } from './../products.js';
+import * as app from './../app.js';
+
+export default {
+  name: 'FilteredProducts',
+  props: ['category'],
+
+  data: function() {
+    return {
+      products: products
+    };
+  },
+
+  mounted() {
+    app.axios.get(app.config.api + 'products').then(response => {
+      this.products = response.data;
+    });
+  },
+
+  computed: {
+    FilteredRecipes: function() {
+      function isMatch(product) {
+        return product.categories.includes(this);
+      }
+      return this.products.filter(isMatch, this.category);
+    }
+  }
+};
+</script>
---
+++ p3/src/main.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+
+import ProductsPage from "./components/pages/ProductsPage.vue";
+import GlutenFreePage from "./components/pages/GlutenFreePage.vue";
+import VeganPage from "./components/pages/VeganPage.vue";
+// import CategoriesPage from './components/pages/CategoriesPage.vue';
+import ProductPage from "./components/pages/ProductPage.vue";
+import Cart from "./components/pages/Cart.vue";
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: "/Vegan", component: VeganPage, name: "vegan" },
+  {
+    path: "/GlutenFree",
+    component: GlutenFreePage,
+    name: "Gluten Free"
+  },
+  { path: "/", component: ProductsPage, name: "recipes" },
+  {
+    path: "/product/:id",
+    component: ProductPage,
+    name: "product",
+    props: true
+  },
+  // { path: '/categories', component: /*CategoriesPage*/, name: 'categories' },
+  { path: "/cart", component: Cart, name: "cart" }
+];
+const router = new VueRouter({
+  routes: routes,
+  mode: "history"
+});
+
+new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount("#app");
