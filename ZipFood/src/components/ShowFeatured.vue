0dd7d76d01649e14bef47a5d9d3418f0045db5e7
mourad <mourad_saleh@harvard.edu>
2019-11-16 19:15:22 -0500

    p3

--- ZipFood/src/components/ShowFeatured.vue
+++ ZipFood/src/components/ShowFeatured.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="featured">
+  <div id='featured'>
     <h2>Featured Products</h2>
-    <ul class="cleanList">
-      <li v-for="product in featuredProducts" :key="product.id">{{ product.name }}</li>
+    <ul class='cleanList'>
+      <li v-for='product in featuredProducts' :key='product.id'>{{ product.name }}</li>
     </ul>
   </div>
 </template>
 <script>
-import { products } from "./../products.js";
+import { products } from './../products.js';
 export default {
-  name: "ShowFeatured",
-  props: ["category"],
+  name: 'ShowFeatured',
+  props: ['category'],
 
   computed: {
     featuredProducts: function() {
