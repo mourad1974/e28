616be0a5d7d7f686b46f5ba6e74984dbe4e3e713
mourad <mourad_saleh@harvard.edu>
2019-11-16 22:51:41 -0500

    new p3

---
+++ p3/src/app.js
@@ -0,0 +1,8 @@
+export { default as Cart } from "./Cart";
+export const axios = require("axios");
+export const config = {
+  api: "https://my-json-server.typicode.com/mourad1974/e28-p3-api/"
+};
+export let store = {
+  cartCount: 0
+};
