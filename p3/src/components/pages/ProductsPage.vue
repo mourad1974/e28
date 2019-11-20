e8c987996672f5d95c076bdbb8e1f1d1439f5f3b
mourad <mourad_saleh@harvard.edu>
2019-11-20 09:06:18 -0500

    fixed filtered recipes loop with the help of the instructor

---
+++ p3/src/components/pages/ProductsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>All Recipes</h2>
+    <div id='products'>
+      <recipe-detail v-for='product in products' :key='product.id' :product='product'></recipe-detail>
+    </div>
+  </div>
+</template>  
+<script>
+import RecipeDetail from './../RecipeDetail.vue';
+import * as app from './../../app.js';
+export default {
+  name: 'Productspage',
+  components: { RecipeDetail },
+  data: function() {
+    return {
+      products: null
+    };
+  },
+  mounted() {
+    app.axios.get(app.config.api + 'products').then(response => {
+      this.products = response.data;
+    });
+  }
+};
+</script>
+<style scoped>
+</style>
